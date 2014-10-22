728c291f36d5ce1a8c1745f7d3cdc79f183d8d0b
GatorDog <GatorDog@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-22 13:08:26 +0000

    tagging version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/cfs-custom-category-fields/tags/1.1@1012103 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/VirtualPosts.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Virtual Posts
+ * 
+ * A class for handling virtual posts for category and taxonomy meta data.
+ * 
+ * Copyright(c) 2014 Schuyler W Langdon
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ *      
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+class VirtualPosts{
+
+    private $postType;
+    private $taxonomy;
+
+    public function __construct($postType, $taxonomy){
+        $this->postType = $postType;
+        $this->taxonomy = $taxonomy;
+    }
+
+    public function registerPostType(){
+        if(post_type_exists($this->postType)){
+            return;
+        }
+        register_post_type($this->postType, array(
+            'public'             => false,
+            'publicly_queryable' => false,
+            'exclude_from_search' => true,
+            'has_archive'        => false,
+            'hierarchical'       => false,
+        ));
+        register_taxonomy($this->taxonomy, $this->postType,
+            array(
+                'public'                => false,
+                'hierarchical'          => false,
+                'show_ui'               => false,
+                'show_admin_column'     => false
+            )
+        );
+        register_taxonomy_for_object_type($this->taxonomy, $this->postType);
+    }
+
+    public function getPost($termId, $flush = false){
+        if(isset($this->posts[$termId]) && !$flush){
+            return $this->posts[$termId]; 
+        }
+        $post = get_posts(array('post_type' => $this->postType, 'posts_per_page' => 1,
+            'tax_query' => array(
+                array(
+                    'taxonomy' => $this->taxonomy,
+                    'field' => 'slug',
+                    'terms' => $termId,
+                )
+            )
+        ));
+        return $this->posts[$termId] = empty($post) ? false : current($post);
+    }
+
+    public function setPost($termId){
+        if(false !== $this->getPost($termId)){
+            return $this->posts[$termId];
+        }
+        $result = wp_insert_post(array(
+            'post_type' => $this->postType,
+            'post_status' => 'publish',
+            'tax_input' => array($this->taxonomy => $termId))
+        );
+        return 0 === $result ? false : get_post($result);
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== CFS Custom Category Fields ===
+Contributors: GatorDog
+Donate link: http://gatordev.com/
+Tags: category meta data, category custom fields, custom field suite addon
+Requires at least: 3.6
+Tested up to: 4.0
+Stable tag: 1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A Custom Field Suite Addon that provides custom meta data for categories and custom taxonomies.
+
+== Description ==
+
+CFS Category Fields is a Custom Field Suite addon that provides meta data or custom fields for categories and custom taxonomies. Key features are as follows:
+
+*   Apply Custom Fields to Categories and Custom Taxonomies
+*   Requires the Custom Field Suite Plugin
+
+== Screenshots ==
+
+1. Apply Field Group to Category or Taxonomy
+
+== Installation ==
+
+1. Upload `cfs-taxonomy.zip` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Install and activate Custom Field Suite if not already installed
+4. On the Field Group editor you will see a box to apply the Field Group to a Category or Taxonomy
+5. Update the Field Group and the fields will be editable on the Wordpress Category Editor
+
+== Frequently Asked Questions ==
+
+= How do I retrieve custom fields for display on a category page? =
+
+Use the function get_category_meta('field_name'), or alternatively, call CfsTaxonomy::get('field_name') directly, in your php.
+
+= How do I retrieve custom category fields anywhere? =
+
+Use the function get_category_meta() and pass it the term object as the second parameter, eg: get_category_meta(false, get_term_by('slug', 'good-stuff', 'category)). This will return all field data from the category Good Stuff. 
+
+= How do I retrieve the cfs front-end form? =
+
+Use the function get_category_form(). If used on a category or archive page it takes no parameters. It can be used anywhere by passing the term object as the first parameter.
+
+== Changelog ==
+
+= 1.1 =
+* Introduces the function get_category_form() which retrieves the cfs front-end form for your field group.
+= 1.0.1 =
+* Flatten field data returned by get_category_meta() when all fields are returned.
+= 1.0 =
+* Initial Release.
